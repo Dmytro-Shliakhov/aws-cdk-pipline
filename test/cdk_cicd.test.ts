8200531411e278e050f0612fd1cecdc7cff71d42
Dmytro-Shliakhov <d.shlyakhov85@gmail.com>
2024-07-31 12:33:02 +0200

    Added unit tests

--- test/cdk_cicd.test.ts
+++ test/cdk_cicd.test.ts
@@ -1,1 +1,9 @@
-test('initial test', () => {});
+import { handler } from "../services/hello";
+
+describe('Hello lambda describe test suite', () => {
+  test('Should return status code 200', async () => {
+    const result = await handler({}, {});
+
+    expect(result.statusCode).toBe(200);
+  });
+});
